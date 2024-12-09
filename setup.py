51abb8aa3cac0b912b1c1dfc9cb444d3dc2a4086
Anshumanjain5 <anshumanjain8886@gmail.com>
2024-12-09 16:35:50 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+def get_packages(path):
+    """
+    *Returns a list of packages present in the given path.*
+    """
+    with open(path, 'r') as f:
+        requirements =  [line.strip() for line in f.readlines() if line.strip()] 
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    
+    return requirements
+
+setup(
+    name="MlProject",
+    version="0.0.1",
+    author="Anshuman jain",
+    author_email="anshumanjain8886@gmail.com",
+    packages=find_packages(),
+    install_requires=get_packages("requirements.txt"),
+)
